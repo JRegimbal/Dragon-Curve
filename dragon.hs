80e88686a305a1305fca28f9d14bc5a249643823
Juliette Regimbal <juliette.regimbal@gmail.com>
2016-12-02 02:45:18 -0500

    Added comments, tried to do some optimization

--- dragon.hs
+++ dragon.hs
@@ -1,22 +1,38 @@
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
+-- Number of iterations
+iterations :: Integer
+iterations = 18
+
+-- Returns a vector rotated by 90-deg or -90-deg
 turnVector :: Floating t => V2 t -> Bool -> V2 t
 turnVector lastDir turnRight =
     if turnRight 
         then lastDir # rotateBy (1/4)
         else lastDir # rotateBy (-1/4)
 
-curveMaker :: Floating t => [Bool] -> V2 t -> [V2 t]
-curveMaker [] vLast = []
-curveMaker (x:xs) vLast =
+-- Creates a curve using the series of turns - True -> right, False -> left
+curveMaker' :: Floating t => [Bool] -> V2 t -> [V2 t]
+curveMaker' [] _ = []
+curveMaker' (x:xs) vLast =
     let vCurrent = (turnVector vLast x)
-    in vCurrent:(curveMaker xs vCurrent)
+    in vCurrent:(curveMaker' xs vCurrent)
+
+curveMaker :: Floating t => [Bool] -> [V2 t]
+curveMaker x = curveMaker' x unit_X
+
+-- Generates the list of turns in a dragon curve of length x
+dragon' :: Integer -> Integer -> [Bool] -> [Bool]
+dragon' count max last
+    | count < max = let tailEnd = reverse . map not $ last
+        in dragon' (count + 1) max (last ++ [True] ++ tailEnd)
+    | otherwise = last
 
 dragon :: Integer -> [Bool]
-dragon 1 = [True]
-dragon x = 
-    let lastDragon = (dragon (x-1))
-    in lastDragon ++ [True] ++ (reverse (map not lastDragon))
+dragon x = dragon' 1 x []
 
-main = mainWith (fromOffsets (curveMaker (dragon 10) unitY) # bg white :: Diagram B)
+diagram :: Diagram B
+diagram = bg white (fromOffsets . curveMaker . dragon $ iterations) 
+
+main = mainWith diagram
