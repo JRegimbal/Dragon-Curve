849ee4bf4b9ada36e5ba8c95ca2443b8e2c1aaa2
Juliette Regimbal <juliette.regimbal@gmail.com>
2016-12-02 03:47:22 -0500

    Added scaling to the vectors

--- dragon.hs
+++ dragon.hs
@@ -3,7 +3,12 @@ import Diagrams.Backend.SVG.CmdLine
 
 -- Number of iterations
 iterations :: Integer
-iterations = 18
+iterations = 20 
+
+-- Scaling factor
+factor :: (Real a, Fractional a) => a
+factor = 1000
+
 
 -- Returns a vector rotated by 90-deg or -90-deg
 turnVector :: Floating t => V2 t -> Bool -> V2 t
@@ -13,14 +18,14 @@ turnVector lastDir turnRight =
         else lastDir # rotateBy (-1/4)
 
 -- Creates a curve using the series of turns - True -> right, False -> left
-curveMaker' :: Floating t => [Bool] -> V2 t -> [V2 t]
+curveMaker' :: (Real t, Floating t) => [Bool] -> V2 t -> [V2 t]
 curveMaker' [] _ = []
 curveMaker' (x:xs) vLast =
     let vCurrent = (turnVector vLast x)
     in vCurrent:(curveMaker' xs vCurrent)
 
-curveMaker :: Floating t => [Bool] -> [V2 t]
-curveMaker x = curveMaker' x unit_X
+curveMaker :: (Real t, Floating t) => [Bool] -> [V2 t]
+curveMaker x = curveMaker' x (factor *^ unit_X)
 
 -- Generates the list of turns in a dragon curve of length x
 dragon' :: Integer -> Integer -> [Bool] -> [Bool]
